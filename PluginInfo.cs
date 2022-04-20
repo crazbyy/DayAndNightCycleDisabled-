43a9d9304dc6868cea37c324087cc868229c752a
crazbyy <104044667+crazbyy@users.noreply.github.com>
2022-04-20 09:01:40 -0500

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NoStreetLights_
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.crazbyy.gorillatag.DayAndNightDisabled";
+        public const string Name = "DayAndNightDisabled";
+        public const string Version = "1.0.0";
+    }
+}
